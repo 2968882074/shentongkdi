68eeedf821c06e8f999f4de6721f46b9f39a5f96
2968882074 <2968882074@.qq.com>
2020-12-25 11:19:43 +0800

    申通快递后台

---
+++ src/test/java/com/yidu/shentongkdi/ShentongkdiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yidu.shentongkdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShentongkdiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
