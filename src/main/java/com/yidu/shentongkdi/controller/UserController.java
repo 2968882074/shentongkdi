2e3e6c10f290b572c8bfd00e31bfea67d937784a
2629504360 <2629504360@qq.com>
2020-12-28 13:47:13 +0800

    用户表

---
+++ src/main/java/com/yidu/shentongkdi/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.yidu.shentongkdi.controller;
+
+import com.yidu.shentongkdi.entity.User;
+import com.yidu.shentongkdi.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+/**
+ * (User)表控制层
+ *
+ * @author makejava
+ * @since 2020-12-28 13:45:32
+ */
+@RestController
+@RequestMapping("user")
+public class UserController {
+    /**
+     * 服务对象
+     */
+    @Resource
+    private UserService userService;
+
+    /**
+     * 通过主键查询单条数据
+     *
+     * @param id 主键
+     * @return 单条数据
+     */
+    @GetMapping("selectOne")
+    public User selectOne(Integer id) {
+        return this.userService.queryById(id);
+    }
+
+}
